bdaf859f7ce33b18e506024c86baa8d57388324d
edit <trkporter@ucdavis.edu>
2021-04-29 20:34:22 -0700

    Try websockets

--- src/lib/CeloProvider.ts
+++ src/lib/CeloProvider.ts
@@ -7,60 +7,97 @@ export class CeloProvider extends providers.JsonRpcProvider {
     network?: providers.Networkish
   ) {
     super(url, network);
-
-    // Override certain block formatting properties that don't exist on Celo blocks
-    // Reaches into https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/formatter.ts
-    const blockFormat = this.formatter.formats.block;
-    blockFormat.gasLimit = () => BigNumber.from(0);
-    blockFormat.nonce = () => "";
-    blockFormat.difficulty = () => 0;
+    setBlockFormat.bind(this)()
   }
 
-  /**
-   * Override to parse transaction correctly
-   * https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/base-provider.ts
-   */
-  async sendTransaction(
+  sendTransaction: (
     signedTransaction: string | Promise<string>
-  ): Promise<providers.TransactionResponse> {
-    await this.getNetwork();
-    const signedTx = await Promise.resolve(signedTransaction);
-    const hexTx = utils.hexlify(signedTx);
-    const tx = parseCeloTransaction(signedTx);
-    try {
-      const hash = await this.perform("sendTransaction", {
-        signedTransaction: hexTx,
-      });
-      return this._wrapTransaction(tx, hash);
-    } catch (error) {
-      error.transaction = tx;
-      error.transactionHash = tx.hash;
-      throw error;
-    }
+  ) => Promise<providers.TransactionResponse> = sendTransaction.bind(this)
+
+  getGasPrice: (feeCurrencyAddress?: string) => Promise<BigNumber> = getGasPrice.bind(this)
+
+  prepareRequest: (method: any, params: any) => [string, Array<any>] = prepareRequest.bind(this, super.prepareRequest.bind(this))
+}
+
+export class CeloWebsocketProvider extends providers.WebSocketProvider {
+  constructor(
+    url: string,
+    network?: providers.Networkish
+  ) {
+    super(url, network);
+    setBlockFormat.bind(this)()
   }
 
-  /**
-   * Override to handle alternative gas currencies
-   * getGasPrice in https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/base-provider.ts
-   */
-  async getGasPrice(feeCurrencyAddress?: string) {
-    await this.getNetwork();
-    const params = feeCurrencyAddress ? { feeCurrencyAddress } : {};
-    return BigNumber.from(await this.perform("getGasPrice", params));
+  sendTransaction: (
+    signedTransaction: string | Promise<string>
+  ) => Promise<providers.TransactionResponse> = sendTransaction.bind(this)
+
+  getGasPrice: (feeCurrencyAddress?: string) => Promise<BigNumber> = getGasPrice.bind(this)
+
+  prepareRequest: (method: any, params: any) => [string, Array<any>] = prepareRequest.bind(this, super.prepareRequest.bind(this))
+}
+
+/**
+ * Override certain block formatting properties that don't exist on Celo blocks
+ * Reaches into https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/formatter.ts
+ */
+export function setBlockFormat(this: providers.JsonRpcProvider) {
+  const blockFormat = this.formatter.formats.block;
+  blockFormat.gasLimit = () => BigNumber.from(0);
+  blockFormat.nonce = () => "";
+  blockFormat.difficulty = () => 0;
+}
+
+/**
+ * Override to parse transaction correctly
+ * https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/base-provider.ts
+ */
+export async function sendTransaction(
+  this: providers.JsonRpcProvider,
+  signedTransaction: string | Promise<string>
+): Promise<providers.TransactionResponse> {
+  await this.getNetwork();
+  const signedTx = await Promise.resolve(signedTransaction);
+  const hexTx = utils.hexlify(signedTx);
+  const tx = parseCeloTransaction(signedTx);
+  try {
+    const hash = await this.perform("sendTransaction", {
+      signedTransaction: hexTx,
+    });
+    return this._wrapTransaction(tx, hash);
+  } catch (error) {
+    error.transaction = tx;
+    error.transactionHash = tx.hash;
+    throw error;
+  }
+}
+
+/**
+ * Override to handle alternative gas currencies
+ * getGasPrice in https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/base-provider.ts
+ */
+export async function getGasPrice(this: providers.JsonRpcProvider, feeCurrencyAddress?: string) {
+  await this.getNetwork();
+  const params = feeCurrencyAddress ? { feeCurrencyAddress } : {};
+  return BigNumber.from(await this.perform("getGasPrice", params));
+}
+
+/**
+ * Override to handle alternative gas currencies
+ * prepareRequest in https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/json-rpc-provider.ts
+ */
+export function prepareRequest<T extends providers.JsonRpcProvider>(
+  this: T,
+  superPrepareRequest: (method: any, params: any) => [string, Array<any>],
+  method: any,
+  params: any
+): [string, Array<any>] {
+  if (method === "getGasPrice") {
+    const param = params.feeCurrencyAddress
+      ? [params.feeCurrencyAddress]
+      : [];
+    return ["eth_gasPrice", param];
   }
 
-  /**
-   * Override to handle alternative gas currencies
-   * prepareRequest in https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/json-rpc-provider.ts
-   */
-  prepareRequest(method: any, params: any): [string, Array<any>] {
-    if (method === "getGasPrice") {
-      const param = params.feeCurrencyAddress
-        ? [params.feeCurrencyAddress]
-        : [];
-      return ["eth_gasPrice", param];
-    }
-
-    return super.prepareRequest(method, params);
-  }
+  return superPrepareRequest(method, params);
 }
